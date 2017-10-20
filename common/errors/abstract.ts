ecffdefe8080283304273bf18252921c642dcf12
Carl Evans <carl@tigatech.co.uk>
2017-10-20 12:15:37 +0100

    clear out master

--- common/errors/abstract.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from './BaseError'
-
-abstract class ConflictError extends BaseError {}
-abstract class NotFoundError extends BaseError {}
-
-export {
-  ConflictError,
-  NotFoundError
-}
